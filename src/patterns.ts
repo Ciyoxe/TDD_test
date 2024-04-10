bf81146eef3491d841ddf06dcb416218257b20cb
ciyoxe <ciyoxe@gmail.com>
2024-04-10 20:03:09 +0700

    is_identifier impl

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,4 +1,4 @@
 // проверка, является ли строка валидным идентификатором для c++
 export function is_identifier(ident: string) {
-    
+    return ident.match(/^[a-zA-Z_]+[a-zA-Z0-9_]*$/) !== null;
 }
